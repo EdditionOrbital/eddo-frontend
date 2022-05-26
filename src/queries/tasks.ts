640da14769afec74923a730a4b6bb63ba1cab5f0
sheshenk <23496040+sheshenk@users.noreply.github.com>
2022-05-26 22:01:58 +0800

    combined all task queries

---
+++ src/queries/tasks.ts
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+
+export const CURRENT_USER_TASKS = gql`
+    query CurrentUserTasks {
+        currentUserTasks {
+            _id
+            title
+            status
+            date
+        }
+    }
+`
+
+export const NEW_TASK = gql`
+    mutation NewTask($title: String, $status: String) {
+        newTask(title: $title, status: $status) {
+			completed
+			error
+		}
+    }
+`
+
+export const UPDATE_TASK = gql`
+    mutation UpdateTask($_id: ID!, $title: String, $status: String) {
+        updateTask(_id: $_id, title: $title, status: $status) {
+            completed
+            error
+        }
+    }
+`
+
+export const DELETE_TASK = gql`
+    mutation DeleteTask($_id: ID!) {
+        deleteTask(_id: $_id) {
+            completed
+            error
+        }
+    }
+`
