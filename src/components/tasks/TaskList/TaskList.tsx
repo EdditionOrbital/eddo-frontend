2eded4347c48837511ad826c2773c289cca75e44
sheshenk <23496040+sheshenk@users.noreply.github.com>
2022-05-26 22:03:24 +0800

    combined callbacks into single object

---
+++ src/components/tasks/TaskList/TaskList.tsx
@@ -0,0 +1,53 @@
+import { ActionIcon, Group, Stack, Text, Title } from "@mantine/core"
+import { useEffect, useState } from "react"
+import TaskModal from "../TaskModal/TaskModal"
+import TaskItem from "../TaskItem/TaskItem"
+import { FaPlus } from 'react-icons/fa'
+import { useQuery } from "@apollo/client"
+import { CURRENT_USER_TASKS } from "../../../queries/tasks"
+import { Task } from "../../../types/task.type"
+
+const emptyTask = { _id: null, title: '', status: 'Not Started'}
+
+const TaskList = () => {
+
+    const [tasks, setTasks] = useState<Task[]>([])
+    const [shownTask, setShownTask] = useState<Task | null>(null)
+    const {loading, error, data} = useQuery(CURRENT_USER_TASKS)
+
+    const addTask = (t: Task) => setTasks([...tasks, t])
+    const updateTask = (t: Task) => setTasks(tasks.map(i => t._id === i._id ? t : i))
+    const deleteTask = (_id: string) => setTasks(tasks.filter(i => _id !== i._id))
+
+    useEffect(() => {
+        var res = []
+        if (loading) {}
+        else if (error) console.log(error)
+        else try { res = data.currentUserTasks } catch {}
+        setTasks(res)
+    }, [loading, error, data])
+
+    useEffect(() => {
+        console.log('Task updated')
+    }, [tasks])
+
+    return (
+        <>
+            <TaskModal task={shownTask} close={() => setShownTask(null)} callbacks={{ add: addTask, update: updateTask, delete: deleteTask}}/>
+            <Stack>
+                <Group position='apart'>
+                    <Title order={2}>Your Tasks</Title>
+                    <ActionIcon variant="light" onClick={() => setShownTask(emptyTask)}><FaPlus/></ActionIcon>
+                </Group>
+                <Stack>
+                    { tasks.length > 0 ? tasks.map(t => <TaskItem onClick={() => setShownTask(t)} task={t}/>) : <Text>You have no pending tasks.</Text>}
+
+                </Stack>
+                {/* <Button variant='outline' style={{width:150}}>View all tasks</Button> */}
+            </Stack>
+        </>
+    )
+
+}
+
+export default TaskList
