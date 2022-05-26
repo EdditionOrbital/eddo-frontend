7638cbaa31f7bab09e600794b701b4f0fefe4852
sheshenk <23496040+sheshenk@users.noreply.github.com>
2022-05-26 22:03:35 +0800

    Create TaskItem.tsx

---
+++ src/components/tasks/TaskItem/TaskItem.tsx
@@ -0,0 +1,17 @@
+import { Card, Stack, Text } from "@mantine/core"
+import { Task } from "../../../types/task.type"
+import TaskCompletionBadge from "../TaskCompletionBadge/TaskCompletionBadge"
+
+const TaskItem = ({task, onClick} : {task : Task, onClick: () => void}) => {
+
+    return (
+        <Card withBorder onClick={onClick} className="fade-hover-card">
+            <Stack align='flex-start' spacing={6}>
+                <Text size='md' style={{cursor:'pointer'}}>{task.title}</Text>
+                <TaskCompletionBadge status={task.status} size='sm'/>
+            </Stack>
+        </Card>
+    )
+}
+
+export default TaskItem
