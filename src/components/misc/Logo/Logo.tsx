057d64206b142a8eabdf7499e4ed67b63d313727
Shashank Acharya <acharya.s@icloud.com>
2022-06-09 21:47:43 +0800

    remove redundant console log

--- src/components/misc/Logo/Logo.tsx
+++ src/components/misc/Logo/Logo.tsx
@@ -2,7 +2,6 @@ import { Image } from "@mantine/core"
 import LogoFull from './eddo-logo-full.svg'
 
 const Logo = ({height} : {height: number}) => {
-    console.log(LogoFull)
     return (
         <Image height={height} width={height * 4.125} fit='contain' src={LogoFull}/>
     )
