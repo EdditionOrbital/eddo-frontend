b695bae97ef722b2099115b08a29520fded36440
Shashank Acharya <acharya.s@icloud.com>
2022-06-12 11:24:39 +0800

    removed HomeWelcome

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,15 @@
-import { Container, SimpleGrid, Stack } from "@mantine/core"
+import { Container, SimpleGrid } from "@mantine/core"
 import AnnouncementList from "../../components/announcements/AnnouncementList/AnnouncementList"
 import CalendarView from "../../components/calendars/CalendarView/CalendarView"
-import HomeWelcome from "../../components/misc/HomeWelcome/HomeWelcome"
 import TaskList from "../../components/tasks/TaskList/TaskList"
 import { User } from "../../types/user.type"
 
 const HomePage = ({user} : {user: User | null | undefined}) => {
     return (
         <Container p={24} size={1600}>
-            <SimpleGrid spacing={120} breakpoints={[{minWidth: 'sm', cols: 2}, {minWidth: 'lg', cols: 3}]}>
-                <Stack spacing={36}>
-                    <HomeWelcome firstName={user?.firstName} lastName={user?.lastName || ''}/>
-                    <TaskList/>
-                </Stack>
+            <SimpleGrid spacing='xl' breakpoints={[{minWidth: 'sm', cols: 2}, {minWidth: 'lg', cols: 3}]}>
                 <CalendarView/>
+                <TaskList/>
                 <AnnouncementList/>
             </SimpleGrid>
         </Container>
