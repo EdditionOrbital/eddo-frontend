b58289d36cac334fc861ef59a33b4d21fb03ec0e
Shashank Acharya <acharya.s@icloud.com>
2022-06-12 19:47:18 +0800

    added logout feature to settings page

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,30 @@ import AppContainer from "./components/navigation/AppContainer/AppContainer";
 import AllModulesPage from "./pages/ModulesPage/ModulesPage";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import { AUTH_TOKEN } from "./utils/constants";
 import { APP_CONTEXT } from "./queries/eddoAppContext";
 import SettingsPage from "./pages/SettingsPage/SettingsPage";
 import { EddoAppContext } from "./types/eddoAppContext.type";
 
 function App() {
 
-  const [user, setUser] = useState<EddoAppContext | undefined>(undefined)
+  const [appContext, setAppContext] = useState<EddoAppContext | undefined>(undefined)
   const {loading, error, data} = useQuery(APP_CONTEXT)
-
-  const logOut = () => {
-    setUser(undefined)
-    localStorage.removeItem(AUTH_TOKEN)
-    window.location.reload()
-  }
 
   useEffect(() => {
     if (data === undefined) return
-    else setUser(data.eddoAppContext)
+    else setAppContext(data.eddoAppContext)
   }, [loading, error, data])
 
-  if (user === undefined) return <></>
-  if (user.currentUser === null) return <LoginPage/>
+  if (appContext === undefined) return <></>
+  if (appContext.currentUser === null) return <LoginPage/>
 
   const props = {
-    user: user.currentUser
+    user: appContext.currentUser,
+    setAppContext: setAppContext
   }
 
   return (
-    <AppContainer logout={logOut}>
+    <AppContainer {...props}>
       <Routes>
         <Route path="/" element={<HomePage {...props}/>}/>
         <Route path="modules" element={<AllModulesPage/>}/>
--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,8 +1,11 @@
-import { Button, Container, Space, Stack, TextInput, Title } from "@mantine/core"
+import { Box, Button, Space, Stack, TextInput, Title } from "@mantine/core"
 import { useForm } from "@mantine/form"
+import React from "react"
+import { EddoAppContext } from "../../types/eddoAppContext.type"
 import { User } from "../../types/user.type"
+import { AUTH_TOKEN } from "../../utils/constants"
 
-const SettingsPage = ({user} : { user : User | null | undefined } ) => {
+const SettingsPage = ({user, setAppContext} : { user : User | null | undefined, setAppContext: React.Dispatch<React.SetStateAction<EddoAppContext | undefined>> } ) => {
 
     const form = useForm({
         initialValues: {
@@ -14,11 +17,16 @@ const SettingsPage = ({user} : { user : User | null | undefined } ) => {
     const handleUpdateAccount = () => console.log('Update account')
     const handleUpdatePassword = () => console.log('Update password')
 
+    const logOutHandler = () => {
+        setAppContext({currentUser: null, dbInitialised: true})
+        localStorage.removeItem(AUTH_TOKEN)
+        window.location.reload()
+    }
+
     return (
-        <Container p={24} style={{ width: '90%', maxWidth: 700 }}>
+        <Box p={24} style={{ width: '90%', maxWidth: 500 }}>
             <Stack>
                 <Title order={2}>Profile Settings</Title>
-                <Space/>
                 <TextInput key='FirstNameInput' placeholder={user?.firstName || ''} label="First Name" size='md' required {...form.getInputProps('firstName')}/>
                 <TextInput key='LastNameInput' placeholder={user?.lastName || ''} label="Last Name" size='md' required {...form.getInputProps('lastName')}/>
                 <TextInput key='EmailInput' placeholder={user?.email || ''} label="Email" size='md' required {...form.getInputProps('email')}/>
@@ -27,11 +35,14 @@ const SettingsPage = ({user} : { user : User | null | undefined } ) => {
             </Stack>
             <Stack mt={48}>
                 <Title order={2}>Change Password</Title>
-                <Space/>
                 <TextInput key='Password' placeholder={'Password'} label="Password" size='md' required {...form.getInputProps('password')}/>
                 <Button size="md" onClick={handleUpdatePassword}>Change Password</Button>
             </Stack>
-        </Container>
+            <Stack mt={48}>
+                <Title order={2}>Log Out</Title>
+                <Button size="md" color='red' onClick={logOutHandler} variant='light'>Log Out of Eddo</Button>
+            </Stack>
+        </Box>
     )
 
 }
