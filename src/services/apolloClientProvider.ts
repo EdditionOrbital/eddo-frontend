d93f54420eac2eda65a6e7f47f8e7a2cd256c08b
Shashank Acharya <acharya.s@icloud.com>
2022-06-25 11:25:39 +0530

    move constants to respective files

--- src/services/apolloClientProvider.ts
+++ src/services/apolloClientProvider.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context'
-import { AUTH_TOKEN } from '../utils/constants'
+import { AUTH_TOKEN } from '../constants/authToken'
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem(AUTH_TOKEN)
