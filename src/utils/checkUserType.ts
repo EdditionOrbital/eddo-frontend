6bc81ad6acf558f63243affcf9eef9225528db6c
Shashank Acharya <acharya.s@icloud.com>
2022-06-25 11:26:19 +0530

    remove admin and change methods

--- src/utils/checkUserType.ts
+++ src/utils/checkUserType.ts
@@ -1,16 +1,9 @@
 import { User } from "../types/user.type";
 
-export const isAdmin = (user: User | null | undefined) => {
-	if (user === undefined) return false
-	return user?.__typename === 'Admin'
-}
-
 export const isStudent = (user: User | null | undefined) => {
-	if (user === undefined) return false
-	return user?.__typename === 'Student'
+	return !user ? false : user?.__typename === 'Student'
 }
 
 export const isStaff = (user: User | null | undefined) => {
-	if (user === undefined) return false
-	return user?.__typename === 'Staff'
+	return !user ? false : user?.__typename === 'Staff'
 }
