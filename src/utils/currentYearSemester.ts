63dc9d5b4531eb7cf653a178967814afc6364f33
Shashank Acharya <acharya.s@icloud.com>
2022-06-25 11:26:35 +0530

    add method to check if current sem

--- src/utils/currentYearSemester.ts
+++ src/utils/currentYearSemester.ts
@@ -1,3 +1,7 @@
 const currentDate = new Date()
 export const currentSem = currentDate.getMonth() < 6 ? 2 : 1
 export const currentYear = currentSem === 1 ? currentDate.getFullYear() : currentDate.getFullYear() - 1
+export const isCurrentSemMod = (moduleId: string) => {
+	const [code, year, sem] = moduleId.split('-')
+	return parseInt(year) === currentYear && parseInt(sem) === currentSem
+}
