f20c0afd003e92a23bd1083e745a82661f580e36
Shashank Acharya <acharya.s@icloud.com>
2022-07-06 11:21:23 +0800

    test for current semester

--- src/utils/currentYearSemester.ts
+++ src/utils/currentYearSemester.ts
@@ -1,5 +1,5 @@
 const currentDate = new Date()
-export const currentSem = currentDate.getMonth() < 6 ? 2 : 1
+export const currentSem = currentDate.getMonth() < 7 ? 2 : 1
 export const currentYear = currentSem === 1 ? currentDate.getFullYear() : currentDate.getFullYear() - 1
 export const isCurrentSemMod = (moduleId: string) => {
 	const [code, year, sem] = moduleId.split('-')
