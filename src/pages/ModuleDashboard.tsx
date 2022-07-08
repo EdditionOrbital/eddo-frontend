29072e6d25057c17d947502f905676d6046f7f2a
Shashank Acharya <acharya.s@icloud.com>
2022-07-08 19:07:41 +0800

    removed dashboard elements
    will add back at a later stage

--- src/pages/ModuleDashboard.tsx
+++ src/pages/ModuleDashboard.tsx
@@ -1,18 +1,8 @@
-import { Card, SimpleGrid, Stack, Title } from "@mantine/core";
-import { useState } from "react";
+import { Stack } from "@mantine/core";
 
 export default function ModuleDashboard() {
-	const [latestAnnouncements, setLatestAnnouncements] = useState([])
-	const [latestFiles, setLatestFiles] = useState([])
-	const [latestAssignments, setLatestAssignments] = useState([])
 	return (
 		<Stack>
-			<Title order={2}>New Assignments</Title>
-			<SimpleGrid breakpoints={[{ minWidth: 'sm', cols: 1}, {minWidth: 'md', cols: 2}, { minWidth: 'lg', cols: 3}]}>
-				<Card withBorder p='md'>
-					<Title order={4}>Filename</Title>
-				</Card>
-			</SimpleGrid>
 		</Stack>
 	)
 }
